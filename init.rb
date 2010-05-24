d4a1003c028537ab9ef4fa1da91e6e9bb8eadab9
Nathan Weizenbaum <nex342@gmail.com>
2010-05-23 17:08:11 -0700

    Add an error message for Rails 2.3.6.

--- init.rb
+++ init.rb
@@ -1,3 +1,7 @@
+if ActionPack::VERSION::STRING == "2.3.6"
+  raise "rails_xss does not support Rails 2.3.6. Please upgrade to Rails 2.3.6.1 or later."
+end
+
 unless $gems_rake_task
   require 'rails_xss/plugin'
 end
