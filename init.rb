c13920981abf9ed48ee35f976c9e05e79c1874fc
Santiago Pastorino <santiago@wyeworks.com>
2010-05-24 00:42:32 -0300

    We doesn't support versions earlier of 2.3.6 because we use html_safe

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
-if ActionPack::VERSION::STRING == "2.3.6"
-  raise "rails_xss does not support Rails 2.3.6. Please upgrade to Rails 2.3.6.1 or later."
+if ActionPack::VERSION::STRING <= "2.3.6"
+  raise "rails_xss does not support Rails 2.3.6 or earlier versions. Please upgrade to Rails 2.3.6.1 or later."
 end
 
 unless $gems_rake_task
